286f82e82f3f8310faa8c1d163f24dffc6c92ecf
Luciana <96786165+LucianaYovino22@users.noreply.github.com>
2022-08-11 19:45:40 -0300

    make it better

--- src/main/java/com/portfolio/LuYovino/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/LuYovino/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://frontend-argentina.web.app")
 public class AuthController {
 
     @Autowired
