42ad293cce42ff3dbc1bd9ee4db3ff277d39704b
Luciana <96786165+LucianaYovino22@users.noreply.github.com>
2022-08-16 22:20:18 -0300

    make it better

--- src/main/java/com/portfolio/LuYovino/LuYovinoApplication.java
+++ src/main/java/com/portfolio/LuYovino/LuYovinoApplication.java
@@ -17,32 +17,16 @@ public class LuYovinoApplication {
         SpringApplication.run(LuYovinoApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
+     @Bean
+        public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-
-                registry.addMapping("/**").allowedOrigins("https:///");
-            }
+           @Override
+           public void addCorsMappings(CorsRegistry registry) {
+               registry.addMapping("/**")
+               .allowedOrigins("*")
+               .allowedMethods("*")
+               .allowedHeaders("*");
+           }
         };
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https:///"));
-
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 }
